58b15a15bae86c93881aca30bd8ff6217469e904
saikiranboga <saikiranboga110792@gmail.com>
2014-11-23 23:52:21 +0530

    Initial Commit of GMail Project by me. :-)

---
+++ batchparser.js
@@ -0,0 +1,48 @@
+// FROM: https://codereview.appspot.com/110640043/diff/60001/lib/exp/batchparser.js
+/**	
+* @constructor	
+* OData batch request parser.	
+* TODO: ridiculously experimental, dont use on prod.	
+*	
+* @param {String} boundary Multipart boundary.	
+* @param {Stream} buffer   Readable input stream	
+*/	
+function BatchParser(boundary, buffer){
+	this.boundary = boundary;
+	this.buffer = buffer;
+	this.results = [];
+}
+
+/**	
+* Parses the batch body.	
+* @param  {Function} opt_callback Optional callback.	
+*/	
+BatchParser.prototype.parse = function(opt_callback){
+	var lines = this.buffer.split('\r\n'),
+		currentIndex = -1;
+	lines[0] = '--' + this.boundary;
+	lines[lines.length-1] = '--' + this.boundary + '--';
+	for (var i = 0; i < lines.length; i++) {	
+		switch (lines[i]) {	
+			case '--' + this.boundary:	
+				console.log("started " + i);
+				break;	
+			case '--' + this.boundary + '--':	
+				opt_callback && opt_callback(null, this.results);	
+				break;	
+			default:
+				if (/^Content\-ID: response\-/.test(lines[i])) {
+					currentIndex = lines[i].replace('Content-ID: response-', '');
+				}
+				if (lines[i][0] == '{') {
+					this.results[currentIndex] = JSON.parse(lines[i]);
+				}
+				break;	
+		}
+	}
+};	
+
+/**	
+* Export BatchParser	
+*/	
+module.exports = BatchParser;
---
+++ index.js
@@ -0,0 +1,142 @@
+var http = require('http'),
+	https = require('https'),
+	open = require('open'),
+	readline = require('readline'),
+	google = require('googleapis'),
+	OAuth2Client = google.auth.OAuth2,
+	gmail = google.gmail('v1'),
+	formidable = require('formidable'),
+	util = require('util');
+
+var CLIENT_ID = '678036308788-tg49689s46v6tmrar31e3ct7b81qnq68.apps.googleusercontent.com',
+	CLIENT_SECRET = 'F_nARPucHQvKXWk18qaQQrPD',
+	REDIRECT_URL = 'http://localhost:9000/oa';
+	OAtoken = "";
+
+var server = http.createServer(function(req, res){
+	res.writeHead(200, {'Content-Type':'text/html'});
+	res.write("Hello");
+	if(req.url.indexOf('/oa')==0){
+		
+		var code = req.url.slice(9);
+		console.log("Received code: " + code);
+
+		getAccessToken(code, function(){
+			gmail.users.messages.list({ userId: 'me', auth: oauth2Client, labelIds:["INBOX"], maxResults: 10 }, function(err,list){
+				if(err){
+					console.log('An error occured', err);
+					return;
+			  	}
+			  	var msgs = list.messages;
+			  	console.log(JSON.stringify(msgs));
+
+			  	var boundary = "batch_foobarbaz";
+
+			  	var bat = "";
+			  	function makeBatch(msg){
+			  		bat = bat + "--" + boundary + "\n" +
+			  				"Content-Type: application/http\n\n"+
+			  				"GET /gmail/v1/users/me/messages/"+ msg.id +"\n\n";
+			  		if(msgs.length)
+			  			makeBatch(msgs.shift());
+			  		else{
+			  			bat = bat + "--" + boundary + "--";
+			  			execBatch();
+			  		}
+			  	}
+			  	makeBatch(msgs.shift());
+
+			  	function execBatch(){
+				  	var options = {
+				  		host: 'www.googleapis.com',
+				        path: '/batch',
+				        method: 'POST',
+				        accept: '*/*',
+				        headers: {
+				        	'Authorization': 'Bearer ' + OAtoken,
+				        	'Host': 'www.googleapis.com',
+				            'Content-Type': 'multipart/mixed; boundary=' + boundary,
+				            'Content-Length': bat.length
+				        }
+				    };
+				  	var req_post = https.request(options);
+				  	req_post.write(bat);
+				  	req_post.end();
+				  	var details = "";
+				  	req_post.on('response', function(response) {
+				  		response.on('data', function (chunk) {
+				  			details += chunk.toString();
+					    });
+					    response.on('end', function () {
+				    		console.log("end");
+					    	res.end("<pre>" + details + "</pre>");
+					    	process.exit();
+					    });
+					});
+				}
+			});
+		})
+	}
+});
+
+function getAccessToken(code, cb){
+	oauth2Client.getToken(code, function(err, tokens){
+		oauth2Client.setCredentials(tokens);
+		OAtoken = tokens.access_token;
+		cb();
+	});
+}
+
+var oauth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+
+var url = oauth2Client.generateAuthUrl({
+	access_type: 'offline', // will return a refresh token
+	scope: 'https://mail.google.com/'
+});
+
+open(url);
+
+server.listen(9000);
+
+
+/*
+//OAUTH client token
+var tkn = {
+		access_token: 'ya29.xgC4flgrYJVD2nSXX4brhQ1qDpCFk8hwKY7wYPNHArQ5QIZgZZ80k-mq0ge7nJWfN-I7xmvVFLwGAA',
+		token_type: 'Bearer',
+		expiry_date: 1416686180696
+	};
+*/
+
+/*
+//structure of multipart request body
+var bat = "--" + boundary + "\n" +
+  	"Content-Type: application/http\n\n" +
+  	"GET /gmail/v1/users/me/messages/149d8dcb4f788426?format=minimal\n\n" +
+  	"--" + boundary + "\n" +
+  	"Content-Type: application/http\n\n"+
+  	"GET /gmail/v1/users/me/messages/149d8b7c7d078299?format=minimal\n\n"+
+  	"--" + boundary + "--";
+*/
+
+
+
+/* 
+// For fetching single message
+var details = "";
+for(var i in list.messages){
+	gmail.users.messages.get({userId:'me', id:list.messages[i].id, auth: oauth2Client},function(err, msg){
+		if(err){
+			console.log('An error occured', err);
+			return;
+		}
+		details = details + 
+			"From: " + msg.payload.headers.filter(function(v){ return v["name"] == "From"; })[0].value + " | " +
+		  	"To: " + msg.payload.headers.filter(function(v){ return v["name"] == "Delivered-To"; })[0].value + " | " +
+		  	"Date: " + msg.payload.headers.filter(function(v){ return v["name"] == "Date"; })[0].value;
+		if(i==list.messages.length-1){
+			res.end(details);
+		}
+	});
+}
+*/
