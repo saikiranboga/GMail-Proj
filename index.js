01da814136df73fed74027ecc479d42a7047a8d7
saikiranboga <saikiranboga110792@gmail.com>
2014-11-26 01:01:51 +0530

    Flattened the code, more functions and modular.

--- index.js
+++ index.js
@@ -11,7 +11,119 @@ var parser = require('http-string-parser');
 var CLIENT_ID = '678036308788-tg49689s46v6tmrar31e3ct7b81qnq68.apps.googleusercontent.com',
 	CLIENT_SECRET = 'F_nARPucHQvKXWk18qaQQrPD',
 	REDIRECT_URL = 'http://localhost:9000/oa';
-	OAtoken = "";
+	OAtoken = "",
+	boundary = "batch_foobarbaz";
+
+function getBatchOptionsObj(){
+	var batchOptions = {
+		host: 'www.googleapis.com',
+	    path: '/batch',
+	    method: 'POST',
+	    accept: '*/*',
+	    headers: {
+	    	'Authorization': 'Bearer ', // append OAtoken to this when using
+	    	'Host': 'www.googleapis.com',
+	        'Content-Type': 'multipart/mixed; boundary=' + boundary,
+	        'Content-Length': 0 // change this to the batch request length when using
+	    }
+	};
+	return batchOptions;
+}
+
+function getMessagesObject(pageToken){
+	var Obj = {
+		userId: 'me',
+		auth: oauth2Client,
+		labelIds:["INBOX"],
+		maxResults: 100
+	}
+	if(pageToken!=null){
+		Obj.pageToken = pageToken;
+	}
+	return Obj;
+}
+
+//// function getBatchHTTPreqStr(msgs, batString, msg, cb)
+// msgs {@array} list of message objects.
+// batString {@string} string which holds HTTP POST request
+// msg {@obj} current message object.
+// cb {@function} function to call after preparing the POST request string.
+////
+
+function getBatchHTTPreqStr(msgs, batString, msg, cb){
+	batString = batString + "--" + boundary + "\n" +
+			"Content-Type: application/http\n\n"+
+			"GET /gmail/v1/users/me/messages/"+ msg.id +"\n\n";
+	if(msgs.length)
+		getBatchHTTPreqStr(msgs, batString, msgs.shift(), cb);
+	else{
+		batString = batString + "--" + boundary + "--";
+		cb(batString);
+	}
+}
+
+function execBatch_req(batchReqStr, cb){
+  	var batchReqObj = getBatchOptionsObj();
+  	batchReqObj.headers.Authorization = batchReqObj.headers.Authorization + OAtoken;
+  	batchReqObj.headers["Content-Length"] = batchReqStr.length;
+
+  	var req_post = https.request(batchReqObj);
+  	req_post.write(batchReqStr);
+  	req_post.end();
+
+  	var details = "";
+  	req_post.on('response', function(response) {
+  		response.on('data', function (chunk) {
+  			details += chunk.toString();
+	    });
+	    response.on('end', function(){
+    		result = parser.parseResponse(details);
+    		bdy = result.body.split("\r\n");
+    		var res_boundary = bdy[bdy.length-2];
+    		res_boundary = res_boundary.substring(0,res_boundary.length-2);
+    		var objs = [];
+    		var i = 0;
+	    	function next(line){
+	    		i++;
+	    		if(line == res_boundary){
+	    			if(bdy.length)
+						next(bdy.shift());
+	    		}
+	    		else if(line == res_boundary + '--'){
+	    			cb(objs);
+	    			if(bdy.length)
+						next(bdy.shift());
+	    		}
+	    		else if(line != '' && line && line[0] == '{'){
+	    			var hds = JSON.parse(line).payload.headers;
+	    			for(var i=0; i<hds.length; i++){
+	    				if(hds[i].name=="From"){
+	    					objs.push(hds[i].value);
+	    					break;
+	    				}
+	    			}
+					if(bdy.length)
+						next(bdy.shift());
+				}
+				else if(bdy.length)
+					next(bdy.shift());
+	    	}
+	    	next(bdy.shift());
+	    });
+	});
+}
+
+function getMessages(Obj, cb){
+	gmail.users.messages.list(Obj, function(err,list){
+		if(err){
+			console.log('An error occured', err);
+			return;
+	  	}
+	  	// console.log(list);
+	  	var msgs = list.messages;
+	  	cb(msgs, list.nextPageToken);
+	});
+}
 
 var server = http.createServer(function(req, res){
 	res.writeHead(200, {'Content-Type':'text/html'});
@@ -19,92 +131,29 @@ var server = http.createServer(function(req, res){
 	if(req.url.indexOf('/oa')==0){
 		
 		var code = req.url.slice(9);
-		// console.log("Received code: " + code);
 
 		getAccessToken(code, function(){
-			gmail.users.messages.list({ userId: 'me', auth: oauth2Client, labelIds:["INBOX"], maxResults: 100 }, function(err,list){
-				if(err){
-					console.log('An error occured', err);
-					return;
-			  	}
-			  	var msgs = list.messages;
-
-			  	var boundary = "batch_foobarbaz";
-
-			  	var bat = "";
-			  	function makeBatch(msg){
-			  		bat = bat + "--" + boundary + "\n" +
-			  				"Content-Type: application/http\n\n"+
-			  				"GET /gmail/v1/users/me/messages/"+ msg.id +"\n\n";
-			  		if(msgs.length)
-			  			makeBatch(msgs.shift());
-			  		else{
-			  			bat = bat + "--" + boundary + "--";
-			  			execBatch();
-			  		}
-			  	}
-			  	makeBatch(msgs.shift());
-
-			  	function execBatch(){
-				  	var options = {
-				  		host: 'www.googleapis.com',
-				        path: '/batch',
-				        method: 'POST',
-				        accept: '*/*',
-				        headers: {
-				        	'Authorization': 'Bearer ' + OAtoken,
-				        	'Host': 'www.googleapis.com',
-				            'Content-Type': 'multipart/mixed; boundary=' + boundary,
-				            'Content-Length': bat.length
-				        }
-				    };
-				  	var req_post = https.request(options);
-				  	req_post.write(bat);
-				  	req_post.end();
-				  	var details = "";
-				  	var snip;
-				  	req_post.on('response', function(response) {
-				  		response.on('data', function (chunk) {
-				  			details += chunk.toString();
-					    });
-					    response.on('end', function(){
-				    		result = parser.parseResponse(details);
-				    		bdy = result.body.split("\r\n");
-				    		var res_boundary = bdy[bdy.length-2];
-				    		res_boundary = res_boundary.substring(0,res_boundary.length-2);
-				    		var objs = [];
-				    		var i = 0;
-					    	function next(line){
-					    		i++;
-					    		if(line == res_boundary){
-					    			if(bdy.length)
-										next(bdy.shift());
-					    		}
-					    		else if(line == res_boundary + '--'){
-					    			res.end("<pre>"+objs.join("\n")+"</pre>");
-					    			process.exit();
-					    			if(bdy.length)
-										next(bdy.shift());
-					    		}
-					    		else if(line != '' && line && line[0] == '{'){
-					    			var hds = JSON.parse(line).payload.headers;
-					    			for(var i=0; i<hds.length; i++){
-					    				if(hds[i].name=="From"){
-					    					objs.push(hds[i].value);
-					    					break;
-					    				}
-					    			}
-									if(bdy.length)
-										next(bdy.shift());
-								}
-								else if(bdy.length)
-									next(bdy.shift());
-					    	}
-					    	next(bdy.shift());
-					    });
-					});
-				}
-			});
+			getMessages(getMessagesObject(null),function(msgs1, nextPageToken){
+				var bat1 = "";
+			  	getBatchHTTPreqStr(msgs1, bat1, msgs1.shift(), function(batchReqStr1){
+			  		execBatch_req(batchReqStr1,function(objs1){
+			  			console.log("First Batch Completed.");
+			  			res.write("<pre>"+objs1.join("\n")+"</pre>");
+			  			//second batch start
+			  			getMessages(getMessagesObject(nextPageToken),function(msgs2){
+							var bat2 = "";
+						  	getBatchHTTPreqStr(msgs2, bat2, msgs2.shift(), function(batchReqStr2){
+						  		execBatch_req(batchReqStr2,function(objs2){
+						  			console.log("Second Batch Completed.");
+						  			res.end("<pre>"+objs2.join("\n")+"</pre>");
+						  			process.exit();
+						  		});
+						  	});
+						})
+			  			// second batch end
+			  		});
+			  	});
+			})
 		})
 	}
 });
